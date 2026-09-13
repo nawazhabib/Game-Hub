cff2004df36bd62f5d2f2321cf2f49f88fc7e1f4
nawazhabib <roaringhabib64@gmail.com>
2026-09-13 11:49:15 +0600

    Create a custome hook to fatch the games

---
+++ src/hocks/useGames.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        apiClient.get<FetchGamesResponse>('/games', { signal: controller.signal })
+            .then(response => {
+                setGames(response.data.results);
+            })
+            .catch((error: any) => {
+                if (error?.code === 'ERR_CANCELED' || error?.name === 'CanceledError' || error?.name === 'AbortError') return;
+                setError(error.message);
+            });
+        return () => controller.abort();
+    }, []);
+
+    return { games, error };
+}
+
+export default useGames;
